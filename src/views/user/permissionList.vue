a2d22039c44a9342cbb353eb797c1cd39bfa2663
tc <983123596@qq.com>
2022-07-18 00:05:18 +0800

    1

---
+++ src/views/user/permissionList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="view_div">
+        {{name}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "permissionList",
+        // 引用组件
+        components: {},
+        // 上级组件向本页面传递的参数
+        props: {},
+        data() {
+            return {
+                name: "permissionList",
+            };
+        },
+        // 本页面计算属性
+        computed: {},
+        // 本页面监听属性
+        watch: {},
+
+        methods: {
+            // 跳转页面
+            async toNextPage(to) {
+                let parameter = {
+                    path: to,
+                    params: {},
+                }
+                await this.$router.push(parameter);
+            },
+            // 初始化数据
+            init() {
+
+            },
+
+        },
+        mounted() {
+            this.init();
+        },
+        beforeDestroy() {
+        },
+    };
+
+</script>
+
+<style scoped>
+
+</style>
